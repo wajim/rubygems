shyamalschandra/bundler

root is already a Pathname

lib/bundler.rb
@@ -241,7 +241,7 @@ module Bundler
 
     def app_config_path
       if app_config = ENV["BUNDLE_APP_CONFIG"]
-        Pathname.new(app_config).expand_path(root)
+        root.join(app_config)
       else
         root.join(".bundle")
       end

wajim/rubygems

bundler/lib/bundler.rb
@@ -290,7 +290,7 @@ module Bundler
         if app_config_pathname.absolute?
           app_config_pathname
         else
-          app_config_pathname.expand_path(root)
+          root.join(app_config_pathname)
         end
       else
         root.join(".bundle")